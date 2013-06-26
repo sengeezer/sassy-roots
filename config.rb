2ad2f28efd3786a0abcc597ef36690227e216688
Fabio Albertin <albertin@gmail.com>
2013-06-27 00:25:57 +0200

    Starting new plus branch for updated upstream version

--- config.rb
+++ config.rb
@@ -8,18 +8,12 @@ images_dir = "img"
 javascripts_dir = "js"
 fonts_dir = "css/fonts"
 
-output_style = :expanded
+output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
+line_comments = false
 color_output = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
